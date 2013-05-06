491ba541dcb8bcf735f10d979a9c80e61b52f802
Frederic Heem <frederic.heem@gmail.com>
2013-05-06 22:17:44 +0200

    init

---
+++ src/main/java/com/coinfeed/marketfeed/MarketFeedApp.java
@@ -0,0 +1,18 @@
+package com.coinfeed.marketfeed;
+
+public class MarketFeedApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("MarketFeedApp");
+		MarketFeedApp app = new MarketFeedApp();
+		app.run(args);
+		
+	}
+
+	private void run(String[] args){
+		System.out.println("MarketFeedApp run");
+	}
+}
