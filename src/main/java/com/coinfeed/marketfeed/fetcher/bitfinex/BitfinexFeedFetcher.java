4788df2ee50f9cb76be99a5476db46832195449d
Frederic Heem <frederic.heem@gmail.com>
2014-01-05 19:35:10 +0100

    change bitfinex api

--- src/main/java/com/coinfeed/marketfeed/fetcher/bitfinex/BitfinexFeedFetcher.java
+++ src/main/java/com/coinfeed/marketfeed/fetcher/bitfinex/BitfinexFeedFetcher.java
@@ -4,7 +4,7 @@ import com.coinfeed.marketfeed.fetcher.FeedFetcher;
 import com.coinfeed.marketfeed.fetcher.FeedPollerConfig;
 
 public class BitfinexFeedFetcher extends FeedFetcher {
-	private static final String QUERY_BITFINEX = "https://bitfinex.com/api/v1/ticker/btcusd";
+	private static final String QUERY_BITFINEX = "https://api.bitfinex.com/v1/ticker/btcusd";
 	public static final String DRIVER_NAME = "Bitfinex";
 	public BitfinexFeedFetcher(FeedPollerConfig config) {
 		super(config, QUERY_BITFINEX);
